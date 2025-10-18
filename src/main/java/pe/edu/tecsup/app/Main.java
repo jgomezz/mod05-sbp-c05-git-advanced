6b81d969b7522495cc61405d16fc4b714f556e23
jgomezz <jgomezz@gmail.com>
2025-10-17 21:39:48 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/app/Main.java
@@ -0,0 +1,23 @@
+package pe.edu.tecsup.app;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    /*
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        int suma = obtenerSuma(5);
+        System.out.println(suma);
+
+    }
+    */
+    public static int obtenerSuma(int n) {
+        int suma = 0;
+        for (int i = 1; i <= n; i++) {
+                suma += i;
+        }
+        return suma;
+    }
+}
