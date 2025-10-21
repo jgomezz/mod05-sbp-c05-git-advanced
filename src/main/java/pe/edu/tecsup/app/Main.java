63f65b3d1cee574306c62735d5177995a78780d0
jgomezz <jgomezz@gmail.com>
2025-10-20 20:55:06 -0500

    fix clonado

--- src/main/java/pe/edu/tecsup/app/Main.java
+++ src/main/java/pe/edu/tecsup/app/Main.java
@@ -19,7 +19,7 @@ public class Main {
             throw new IllegalArgumentException("El número debe ser no negativo");
         }
 
-        int suma = 0;
+        int suma = 0; // Cambio desde el rep. clonado
         for (int i = 1; i <= n; i++) {
                 suma += i;
         }
